2bdaf233c6315c18ae899545d526cfc5d214af4b
trebloc <ctse77@alum.berkeley.edu>
2016-01-13 15:56:40 -0800

    reorganized routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,17 @@ Rails.application.routes.draw do
   get "/users", to: "users#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"  
   post "/users", to: "users#create"
-  get "/users/:id", to: "users#show", as: "user"  
+  get "/users/:id", to: "users#show", as: "user" 
   get "/users/:user_id/libraries", to: "library_users#index", as: "user_libraries"  
+
   get "/login", to: "sessions#new"
   get "/logout", to: "sessions#destroy" # <-- strictly speaking this isn't RESTful (it should be a DELETE not GET), but it's super conveient to do it this way
-  get "/libraries", to: "libraries#index"
-  get "/libraries/new", to: "libraries#new", as: "new_library"
-  get "/libraries/:id", to: "libraries#show", as: "library"  
-  post "/libraries", to: "libraries#create"
+
   post "/sessions", to: "sessions#create"
-  post "/libraries/:library_id/users", to: "library_users#create", as: "library_users"     
+
+  get "/libraries", to: "libraries#index"
+  post "/libraries", to: "libraries#create"  
+  get "/libraries/new", to: "libraries#new", as: "new_library"
+  get "/libraries/:id", to: "libraries#show", as: "library" 
+  post "/libraries/:library_id/users", to: "library_users#create", as: "library_users"   
 end
