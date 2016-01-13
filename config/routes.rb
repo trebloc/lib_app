f60ac0c0de3a43576ec6df8a65e302f430d58b5b
trebloc <ctse77@alum.berkeley.edu>
2016-01-13 12:07:29 -0800

    created new route, post to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   get "/users", to: "users#index", as: "users"
-
   get "/users/new", to: "users#new", as: "new_user"  
+  post "/users", to: "users#create"
 end
