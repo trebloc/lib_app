138d4a6ad343f177436ea60191cbed4d18c71e54
trebloc <ctse77@alum.berkeley.edu>
2016-01-13 14:45:20 -0800

    added library show page

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -13,7 +13,6 @@ class LibrariesController < ApplicationController
 	end
 
 	def create
-		library_params = params.require(:library).permit(:name, :floor_count, :floor_area)
 		@library = Library.create(library_params)
 
 		redirect_to libraries_path
@@ -22,5 +21,11 @@ class LibrariesController < ApplicationController
  	def show
     	@library = Library.find_by_id(params[:id])
     	render :show
-  	end  	
+  	end
+
+  	private
+
+  	def library_params
+    	params.require(:library).permit(:name, :floor_count, :floor_area)
+  end  	  	
 end
