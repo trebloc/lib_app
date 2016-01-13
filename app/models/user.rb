fc32ff6e150e286c99220c810e4d501d562cab98
trebloc <ctse77@alum.berkeley.edu>
2016-01-13 14:52:36 -0800

    joining library and a user, reciprocal associations in library model, associate library user and user too

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  
+  has_many :library_users
+  has_many :libraries, through: :library_users
+
   has_secure_password
+
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
