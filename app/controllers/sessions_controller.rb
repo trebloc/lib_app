f68996775b6ff7d013fef57292d4db6e6543e9c9
trebloc <ctse77@alum.berkeley.edu>
2016-01-13 13:33:08 -0800

    added the sessions create route and updated the destroy for users controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,16 @@ class SessionsController < ApplicationController
     @user = User.new
     render :new
   end
-  
+
+  def create
+    user_params = params.require(:user).permit(:email, :password)
+    @user = User.confirm(user_params)
+    if @user
+      login(@user)
+      redirect_to @user
+    else
+      redirect_to login_path
+    end
+  end
+    
 end
